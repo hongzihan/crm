864bb84fcd3a4e15f8395f50ded1df05c412fb14
ken <1601790617@qq.com>
2019-01-23 10:44:24 +0800

    给如何通过泛型反射的方式获取class增加了注释

--- crm/src/com/hzh/crm/dao/impl/BaseDaoImpl.java
+++ crm/src/com/hzh/crm/dao/impl/BaseDaoImpl.java
@@ -17,10 +17,15 @@ public class BaseDaoImpl<T> extends HibernateDaoSupport implements BaseDao<T> {
 	
 	// 通过建立构造器来获取class
 	public BaseDaoImpl() {
-		Class clazz = this.getClass();
-		Type type = clazz.getGenericSuperclass();
+		// 反射: 第一步获得Class
+		Class clazz = this.getClass(); // 这里获取到的class是目前正在被调用的class,CustomerDaoImpl或LinkManDaoImpl
+		// 通过查看JDK的API得知可以通过class获取父类的class(这里的Class已经传入泛型T)
+		Type type = clazz.getGenericSuperclass(); // 参数化类型: BaseDaoImpl<Customer>，BaseDaoImpl<LinkMan>
+		// 得到的type本身是一个参数化的类型，所以这里将type强转成参数化的类型
 		ParameterizedType pType = (ParameterizedType) type;
+		// 通过参数化类型获得实际类型参数: 得到一个实际类型参数的数组?Map<String,Integer>(这里的泛型有可能是Map,所以这里获取的是数组)
 		Type[] types = pType.getActualTypeArguments();
+		// 只获得第一个实际类型参数即可
 		this.clazz = (Class) types[0];
 	}
 
